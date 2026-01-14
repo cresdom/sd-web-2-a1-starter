027889aa45c731a9bc46065539b09f3d6b7644aa
cresdom <domcres807@icloud.com>
2026-01-13 23:08:46 -0700

    used filter method for exercise 2

--- script.js
+++ script.js
@@ -30,12 +30,13 @@ characters.forEach(character =>{
 // 2. Filter the characters array to find only those characters whose age property is less than 40. Log each filtered character's name to the console. Then, dynamically create <li> elements for each filtered character and append them to the HTML unordered list element with the id "young-characters-list".
 
 let youngCharacters = document.getElementById("young-characters-list");
+let filteredList = characters.filter(character => character.age < 40);
 
 console.log("Filter by age:");
-characters.forEach(character =>{
-  if (character.age < 40){
-    console.log(character.name)}
-    youngCharacters.innerHTML += `<li>${character.name}</li>\n`
+
+filteredList.forEach(character => {
+  console.log(character.name)
+  youngCharacters.innerHTML += `<li>${character.name}</li>\n`
 });
 
 // 3. Build a reusable function that accepts an array of character objects as a parameter. Inside the function, iterate through the array and extract each character's name property. Dynamically generate <li> elements for each name and append them to a target HTML list element. Call this function with the characters array and render the results in the unordered list with id "function-list".
